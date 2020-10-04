5572e2b3c54c0f59d16f567618fdae9d0acd83be
Martin Darmüntzel <martin@trivialanalog.de>
2020-10-04 14:57:44 +0200

    Ändert die URL zur Fachschaftsapp.

--- fsrmedhro/urls.py
+++ fsrmedhro/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path(
-        'fachschaft/',
+        'app:fachschaft/',
         include(
             'fsmedhro_core.urls',
             namespace='fsmedhro_core'
